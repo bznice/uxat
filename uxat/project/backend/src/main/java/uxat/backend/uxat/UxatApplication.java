f5457f37bbb18aa76dfbf61434d2e20183d48453
bznice <lfm.videira@gmail.com>
2022-12-21 23:12:00 +0000

    created backend template (spring initializr); created css var for font size.

---
+++ uxat/project/backend/src/main/java/uxat/backend/uxat/UxatApplication.java
@@ -0,0 +1,13 @@
+package uxat.backend.uxat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UxatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UxatApplication.class, args);
+	}
+
+}
---
+++ uxat/project/backend/src/test/java/uxat/backend/uxat/UxatApplicationTests.java
@@ -0,0 +1,13 @@
+package uxat.backend.uxat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UxatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
