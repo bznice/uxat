72c7539ad45e9625cc8f824f552878a135332e00
bznice <lfm.videira@gmail.com>
2022-12-26 00:35:32 +0000

    Password validation and lot of other stuff

--- uxat/project/frontend/src/ctrl/uxat.js
+++ uxat/project/frontend/src/ctrl/uxat.js
@@ -1,27 +1,57 @@
 angular.module("uxat", []);
 angular.module("uxat").controller("uxatCtrl", ['$scope', function ($scope) {
 
+    /*************************************************************************/
+    /** DATA */
+
+    $scope.users = [
+        {nick: "bznc", pass: "xxxx1234"},
+        {nick: "viruz", pass: "xxxx1904"},
+        {nick: "bob", pass: "xxxx0420"}
+    ];
+    $scope.usersN = 1;
+
+    const minSizePass = 8;
+    const mandatoryCharsPassLower = "qwertyuiopasdfghjklzxcvbnm";
+    const mandatoryCharsPassUpper = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    const mandatoryCharsPassDigit = "0123456789";
+
     const appBegin = true;
     const arrayTheme = ["light", "dark"];
 
-    $scope.users = [
-        {nick: "bznc", pass: "1234"}
-    ];
-    
-    $scope.usersN = 1;
-
     $scope.passDontMatchSpan = "";
+    const passDontMatchMessage = "Both passwords don't match!";
+    $scope.passSmallSpan = "";
+    const passSmallMessage = "Password mandatory no less than 8";
+    $scope.passNotAllowSpan = "";
+    const passNotAllowMessage = "Password mandatory contain lower, upper, digit, ONLY!";
     $scope.userNotUniqueSpan = "";
+    const userNotUniqueMessage = "This Nick is not available!";
 
     $scope.helloWorldView = appBegin;
     $scope.uXatView = !appBegin;
     $scope.loginView = !appBegin;
     $scope.createView = !appBegin;
 
-    document.body.className = arrayTheme.some(theme => document.body.className.includes(theme))
+    document.body.className = arrayTheme
+        .some(theme => document.body.className.includes(theme))
         ? document.body.className : "dark " + document.body.className;
 
-    /***********************************************************************************************/
+    /*************************************************************************/
+    /** GERAL */
+
+    $scope.setTimeoutSpan = function(element, span) {
+        if(span != "") clearTimeout(span);
+        span = setTimeout($scope.clearDemo, 3000, element, span);
+    }
+
+    $scope.clearDemo = function (log, timeoutId) {
+        log.textContent = "";
+        timeoutId = "";
+    }
+
+    /*************************************************************************/
+    /** INIT */
 
     $scope.go = function() {
         $scope.helloWorldView = false;
@@ -44,29 +74,49 @@ angular.module("uxat").controller("uxatCtrl", ['$scope', function ($scope) {
         $scope.createView = true;
     }
 
-    /***********************************************************************************************/
+    /*************************************************************************/
     /** LOGIN */
 
     $scope.loginUser = function(user) {
         $scope.go();
     }
 
-    /***********************************************************************************************/
+    /*************************************************************************/
     /** CREATE */
 
     $scope.createUser = function(user) {
-        if($scope.passEquals(user.pass, user.passTentative)) {
-            if($scope.verifyNickIsUnique(user.nick)) {
-                $scope.go();
-            } else {
-                $scope.spanErrorNickNotUnique();
-                user.nick = "";
-            }
+        let resultValidation = $scope.createAccountValidation(user);
+        if(resultValidation === 0) {
+            $scope.go();
         } else {
-            $scope.spanErrorPassDontMatch();
-            user.pass = "";
-            user.passTentative = "";
+            switch(resultValidation) {
+                case 1: $scope.spanErrorPassDontMatch();
+                        user.pass = "";
+                        user.passTentative = "";
+                        break;
+                case 2: $scope.spanErrorNickNotUnique();
+                        user.nick = "";
+                        break;
+                case 3: $scope.spanErrorPassSmall();
+                        user.pass = "";
+                        user.passTentative = "";
+                        break;
+                case 4: $scope.spanErrorPassNotAllow();
+                        user.pass = "";
+                        user.passTentative = "";
+                        break;
+                default: ;
+            }
         }
+    }
+
+    $scope.createAccountValidation = function(user) {
+        if(!($scope.passEquals(user.pass, user.passTentative))) return 1;
+        if(!($scope.verifyNickIsUnique(user.nick))) return 2;
+        if(!($scope.passNotSmall(user.pass))) return 3;
+        if(!($scope.passHaveMandatoryChars(user.pass))) return 4;
+
+        return 0;
     }
 
     $scope.passEquals = function(passO, passT) {
@@ -78,27 +128,55 @@ angular.module("uxat").controller("uxatCtrl", ['$scope', function ($scope) {
         return userListResult.length === 0;
     }
 
+    $scope.passNotSmall = function(pass) {
+        return pass.length >= minSizePass;
+    }
+
+    $scope.passHaveMandatoryChars = function(pass) {
+        let containsLower = false;
+        let containsUpper = false;
+        let containsDigit = false;
+        [...mandatoryCharsPassLower].forEach((c) => {
+            if(pass.includes(c)) {
+                containsLower = true;
+            }
+        });
+        [...mandatoryCharsPassUpper].forEach((c) => {
+            if(pass.includes(c)) {
+                containsUpper = true;
+            }
+        });
+        [...mandatoryCharsPassDigit].forEach((c) => {
+            if(pass.includes(c)) {
+                containsDigit = true;
+            }
+        });
+        return containsLower && containsUpper && containsDigit;
+    }
+
     $scope.spanErrorPassDontMatch = function() {
         var spanError = document.getElementById("spanErrorCreate");
-        spanError.textContent = "Both passwords don't match!";
+        spanError.textContent = passDontMatchMessage;
         $scope.setTimeoutSpan(spanError, $scope.passDontMatchSpan);
+    }
+
+    $scope.spanErrorPassSmall = function() {
+        var spanError = document.getElementById("spanErrorCreate");
+        spanError.textContent = passSmallMessage;
+        $scope.setTimeoutSpan(spanError, $scope.passSmallSpan);
+    }
+
+    $scope.spanErrorPassNotAllow = function() {
+        var spanError = document.getElementById("spanErrorCreate");
+        spanError.textContent = passNotAllowMessage;
+        $scope.setTimeoutSpan(spanError, $scope.passNotAllowSpan);
     }
 
     $scope.spanErrorNickNotUnique = function() {
         var spanError = document.getElementById("spanErrorCreate");
-        spanError.textContent = "This Nick is not available!";
+        spanError.textContent = userNotUniqueMessage;
         $scope.setTimeoutSpan(spanError, $scope.userNotUniqueSpan);
     }
 
-    /***********************************************************************************************/
-
-    $scope.setTimeoutSpan = function(element, span) {
-        if(span != "") clearTimeout(span);
-        span = setTimeout($scope.clearDemo, 3000, element, span);
-    }
-
-    $scope.clearDemo = function (log, timeoutId) {
-        log.textContent = "";
-        timeoutId = "";
-    }
+    /*************************************************************************/
 }]);
